e848b79bba56602b4a543f0ab52c50565e9612e6
Eric Bailey <eric@ericb.me>
2024-08-25 01:24:58 -0500

    feat(Destructuring): solve

--- koans/DestructuringKoans.jl
+++ koans/DestructuringKoans.jl
@@ -1,9 +1,12 @@
 module DestructuringKoans
 
 function add_separately(x, y)
+  x_re, x_im = reim(x)
+  y_re, y_im = reim(y)
+  x_re + y_re, x_im + y_im
 end
 
-function add_coordinates(___, ___)
-end
+add_coordinates((x1, y1), (x2, y2)) =
+  x1 + x2, y1 + y2
 
 end
